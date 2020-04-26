4ecc54945807844c2e5583e1097ede529d63a249
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-26 18:49:24 +0000

    Done.

---
+++ mine.rb
@@ -0,0 +1,20 @@
+r1=[1,2,3]
+r2=[4,5,6]
+r3=[7,8,9]
+
+all=[r1,r2,r3]
+
+#print all
+
+all2=[
+[1,2,3],
+[4,5,6],
+[7,8,9]
+]
+
+#print all2
+
+puts all2[2]
+puts all2[0][2]=1111
+#puts all2[3][1]
+print all2
